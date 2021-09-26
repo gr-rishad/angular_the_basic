1e55b956332af441ae97d8f2646d42a21d9fdeb7
Golam Rabbani Rishad <33056647+gr-rishad@users.noreply.github.com>
2021-09-27 00:55:25 +0600

    Bindable properties and Events

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'app-server',
-    templateUrl:'./server.component.html'
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 })
-export class serverComponent{
+export class serverComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
+    getServerStatus(){
+      return  this.serverStatus;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
-  selector:'app-servers',
- // selector: '[app-servers]',
-// selector:'.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector:'.app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus='No Server was Created!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus="Sever was created.";
   }
 
 }
